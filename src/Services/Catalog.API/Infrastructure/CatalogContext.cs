0f6a831c3a10e4900b6431fd2265db1d30d67025
mindtnv <47498401+mindtnv@users.noreply.github.com>
2023-02-16 22:50:24 +0300

    Add CatalogContext

---
+++ src/Services/Catalog.API/Infrastructure/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Infrastructure.EntityConfigurations;
+using Catalog.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Infrastructure;
+
+public class CatalogContext : DbContext
+{
+    public DbSet<CatalogItem> CatalogItems { get; set; } = null!;
+    public DbSet<CatalogBrand> CatalogBrands { get; set; } = null!;
+    public DbSet<CatalogType> CatalogTypes { get; set; } = null!;
+
+    public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new CatalogItemEntityTypeConfiguration());
+        builder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
+        builder.ApplyConfiguration(new CatalogTypeEntityTypeConfiguration());
+    }
+}
