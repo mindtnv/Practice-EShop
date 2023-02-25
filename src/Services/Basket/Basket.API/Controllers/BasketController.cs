b4b173c9d06a408830981c227d0a09c1ff67f062
mindtnv <47498401+mindtnv@users.noreply.github.com>
2023-02-25 13:18:54 +0300

    Add BasketController
    Add BasketControllerTests

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using Basket.API.Infrastructure.Repositories;
+using Basket.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository;
+    }
+
+    [Route("all")]
+    [HttpGet]
+    [ProducesResponseType((int) HttpStatusCode.OK)]
+    public async Task<ActionResult<CustomerBasket[]>> GetCustomerBasketsAsync()
+    {
+        var baskets = new List<CustomerBasket>();
+        await foreach (var basket in _basketRepository.GetBasketsAsync())
+            baskets.Add(basket);
+
+        return Ok(baskets);
+    }
+
+    [HttpPost]
+    [ProducesResponseType((int) HttpStatusCode.OK)]
+    [ProducesResponseType((int) HttpStatusCode.BadRequest)]
+    public async Task<ActionResult> UpdateCustomerBasketAsync([FromRoute] CustomerBasket basket)
+    {
+        var isSuccess = await _basketRepository.UpdateBasketAsync(basket);
+        return isSuccess ? Ok() : BadRequest();
+    }
+
+    [HttpGet]
+    [Route("{customerId}")]
+    [ProducesResponseType((int) HttpStatusCode.OK)]
+    public async Task<ActionResult<CustomerBasket>> GetCustomerBasketByIdAsync(string customerId)
+    {
+        var basket = await _basketRepository.GetBasketAsync(customerId);
+        return basket ?? new CustomerBasket
+        {
+            CustomerId = customerId,
+        };
+    }
+
+    [HttpDelete]
+    [Route("{customerId}")]
+    [ProducesResponseType((int) HttpStatusCode.OK)]
+    [ProducesResponseType((int) HttpStatusCode.NotFound)]
+    public async Task<ActionResult> DeleteCustomerBasketAsync([FromRoute] string customerId)
+    {
+        var isSuccess = await _basketRepository.DeleteBasketAsync(customerId);
+        return isSuccess ? Ok() : NotFound();
+    }
+}
---
+++ src/Services/Basket/Basket.UnitTests/BasketControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using Basket.API.Controllers;
+using Basket.API.Infrastructure.Repositories;
+using Basket.API.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Basket.UnitTests;
+
+[TestFixture]
+public class BasketControllerTests
+{
+    [SetUp]
+    public void SetUp()
+    {
+        _customerId = "customer-id";
+
+        var repositoryMock = new Mock<IBasketRepository>();
+        repositoryMock.Setup(x => x.GetBasketAsync(It.IsAny<string>()))
+                      .ReturnsAsync((string customerId) => GetCustomerBasket(customerId));
+        repositoryMock.Setup(x => x.DeleteBasketAsync(It.IsAny<string>()))
+                      .ReturnsAsync((string x) => x == _customerId);
+        repositoryMock.Setup(x => x.UpdateBasketAsync(It.IsAny<CustomerBasket>()))
+                      .ReturnsAsync((CustomerBasket x) => x.CustomerId == _customerId);
+
+        _repository = repositoryMock.Object;
+        _controller = new BasketController(_repository);
+    }
+
+    private string _customerId = null!;
+    private IBasketRepository _repository = null!;
+    private BasketController _controller = null!;
+
+    private CustomerBasket GetCustomerBasket(string customerId) => new()
+    {
+        CustomerId = customerId,
+        Items = new List<CustomerBasketItem>
+        {
+            new()
+            {
+                ProductId = 0,
+                Quantity = 0,
+                UnitPrice = 0,
+                OldUnitPrice = 0,
+                PictureUrl = "",
+            },
+        },
+    };
+
+    [Test]
+    public async Task DeleteCustomerBasketByIdAsync()
+    {
+        var actionResult = await _controller.DeleteCustomerBasketAsync(_customerId);
+        actionResult.Should().BeOfType<OkResult>();
+        var actionResult2 = await _controller.DeleteCustomerBasketAsync("notfound-id");
+        actionResult2.Should().BeOfType<NotFoundResult>();
+    }
+
+    [Test]
+    public async Task GetCustomerBasketByIdAsync()
+    {
+        var basket = await _repository.GetBasketAsync(_customerId);
+        var actionResult = await _controller.GetCustomerBasketByIdAsync(_customerId);
+        actionResult.Should().BeOfType<ActionResult<CustomerBasket>>();
+        actionResult.Value.Should().NotBeNull();
+        actionResult.Value!.Should().BeEquivalentTo(basket);
+    }
+
+    [Test]
+    public async Task UpdateCustomerBasketByIdAsync()
+    {
+        var actionResult = await _controller.UpdateCustomerBasketAsync(new CustomerBasket
+        {
+            CustomerId = _customerId,
+        });
+        actionResult.Should().BeOfType<OkResult>();
+        var actionResult2 = await _controller.UpdateCustomerBasketAsync(new CustomerBasket
+        {
+            CustomerId = "notfound-id",
+        });
+        actionResult2.Should().BeOfType<BadRequestResult>();
+    }
+}
